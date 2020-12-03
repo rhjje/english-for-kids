ad46710678a90f8ed47b855954f6a136405e97e9
rhjje <numelen@gmail.com>
2020-12-03 21:12:08 +0300

    version 4

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,39 @@ const miniCss = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(s*)css$/,
-        use: [
-          miniCss.loader,
-          'css-loader',
-          'sass-loader',
+    entry: './src/app.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [{
+          test: /\.(s*)css$/,
+          use: [
+            miniCss.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+          },
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
         ],
       },
-      {
-        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-        },
-      },
-    ],
-  },
-  plugins: [
-    new miniCss({
-      filename: 'style.css',
-    }),
-  ],
-};
+      plugins: [
+        new miniCss({
+          filename: 'style.css',
+        }),
+      ],
+    };
